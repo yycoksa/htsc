0bd1bea955718a3ed0bd46f3ffbe707bc402670c
yycoksa <108451733+yycoksa@users.noreply.github.com>
2022-07-06 16:28:40 +0800

    Add files via upload

---
+++ IdeaSettingsRepository-master/fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/** 
+ * @Description TODO
+ *
+ *
+ * @Date ${DATE} ${TIME}
+ * @Author K1131312
+ */
+public class ${NAME} {
+}
